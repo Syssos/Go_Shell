f6b38c2792e1d7f78ba5cfda2b538e7ebf4b19c6
Syssos <paral.cody98@gmail.com>
2021-05-30 19:41:44 -0600

    added 404 functionality

--- settings/GofshTemplates/server.go
+++ settings/GofshTemplates/server.go
@@ -3,6 +3,9 @@ package main
 import (
     "fmt"
     "log"
+    // "os"
+    "io/ioutil"
+    "strings"
     "net/http"
 )
 
@@ -11,9 +14,31 @@ func main() {
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        if r.URL.Path != "/" {
+            errorHandler(w, r, http.StatusNotFound)
+            return
+        }
         http.ServeFile(w, r, "./templates/index.html")
     })
 
     fmt.Println("Starting server on localhost:3000")
     log.Fatal(http.ListenAndServe(":3000", nil))
+}
+
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+    w.WriteHeader(status)
+    fmt.Println(r.URL)
+    dat, err := ioutil.ReadFile("./templates/404.html")
+    checkErr(err)
+
+    pageData := strings.Replace(string(dat), "{{url}}", fmt.Sprintf("%v", r.URL), -1)
+    if status == http.StatusNotFound {
+        fmt.Fprint(w, pageData)
+    }
+}
+
+func checkErr(e error) {
+    if e != nil {
+        fmt.Println(e)
+    }
 }
