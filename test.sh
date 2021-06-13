ccc7b35921e4824220d0240de72387053bb1b047
Syssos <paral.cody98@gmail.com>
2021-06-13 17:29:50 -0600

    accounting for travis checker

--- test.sh
+++ test.sh
@@ -7,12 +7,9 @@ mkdir "$HOME/go/bin"
 
 if [ -d "$DIR" ]; then
   go test -v ./
-  ./clean.sh
 else
   chmod u+x "build.sh"
-  chmod u+x "clean.sh"
   ./build.sh
   sudo cp "$GOPATH/bin/gofsh" "$HOME/go/bin"
   go test -v ./
-  ./clean.sh
 fi
